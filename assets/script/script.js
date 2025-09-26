f46726386d6669b023a0fd4cb9d39feae6cbe2d8
karlvinfdl <karlvinfidelin@gmail.com>
2025-09-26 10:34:07 +0200

    modification et ajout page produit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,20 @@
-const btn = document.getElementById("toggleAmenities");
-const more = document.getElementById("moreAmenities");
 
-btn.addEventListener("click", () => {
-  more.classList.toggle("hidden__detail");
-  btn.textContent = more.classList.contains("hidden__detail")
-    ? "Voir plus"
-    : "Voir moins";
-});
+    // Dropdown équipements
+    const btn = document.getElementById("toggleAmenities");
+    const more = document.getElementById("moreAmenities");
 
+    btn.addEventListener("click", () => {
+      more.classList.toggle("hidden__detail");
+      btn.textContent = more.classList.contains("hidden__detail")
+        ? "Voir plus"
+        : "Voir moins";
+    });
 
-
-// Initialiser la carte (coordonnées = Lyon centre)
-const map = L.map('map').setView([45.758, 4.84], 13);
-
-// Ajouter le fond de carte (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-// Ajouter un marqueur
-L.marker([45.758, 4.84])
-  .addTo(map)
-  .bindPopup("<b>Logement à Lyon</b><br>Proche centre-ville.")
-  .openPopup();
+    // Carte Leaflet
+    const map = L.map('map').setView([45.758, 4.84], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+    L.marker([45.758, 4.84]).addTo(map)
+      .bindPopup("<b>Logement à Lyon</b><br>Proche centre-ville.")
+      .openPopup();
